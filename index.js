d03cb333d121367850c24fce3ea73a56d7744c13
Andrew Nesbitt <andrewnez@gmail.com>
2013-09-30 18:15:15 +0100

    Added map server

--- index.js
+++ index.js
@@ -7,55 +7,6 @@ var client = arDrone.createClient();
 
 client.config('general:navdata_demo', 'FALSE');
 
-client.takeoff()
-
-var targetLat, targetLon, targetYaw, cyaw;
-
-setTimeout(function(){
-  // end of garden: 51.392059 -2.3224394
-  targetLat = 51.392059
-  targetLon = -2.3224394
-}, 6000)
-
-var handleNavData = function(data){
-  if ( data.demo == null) return;
-
-  var currentLat = data.gps.latitude
-  var currentLon = data.gps.longitude
-  console.log('lat/lon:', currentLat, currentLon);
-
-  var currentYaw = data.demo.rotation.yaw;
-
-  if (targetLat == null || targetLon == null || currentYaw ==  null || currentLat == null || currentLon == null) return;
-
-  var bearing = vincenty.distVincenty(currentLat, currentLon, targetLat, targetLon)
-
-  if(bearing.distance > 1){
-    console.log('distance', bearing.distance)
-    console.log('bearing:', bearing.initialBearing)
-    targetYaw = bearing.initialBearing
-
-    console.log('currentYaw:', currentYaw);
-    var eyaw = targetYaw - currentYaw;
-    console.log('eyaw:', eyaw);
-
-    var uyaw = yawPID.getCommand(eyaw);
-    console.log('uyaw:', uyaw);
-
-    var cyaw = within(uyaw, -1, 1);
-    console.log('cyaw:', cyaw);
-
-    client.clockwise(cyaw)
-    client.front(0.05)
-  } else {
-    targetYaw = null
-    client.stop()
-    console.log('Reached ', targetLat, targetLon)
-  }
-}
-
-client.on('navdata', handleNavData);
-
 function within(x, min, max) {
   if (x < min) {
       return min;
@@ -65,4 +16,72 @@ function within(x, min, max) {
       return x;
   }
 }
+
+// client.takeoff()
+
+function gpsNavigator(client){
+  this.targetLat = null;
+  this.targetLon = null;
+  this.targetYaw = null;
+  this.callback = null;
+
+  this.client = client;
+
+  this.waypoint = function(lat, lon, cb){
+    this.targetLat = lat
+    this.targetLon = lon
+    this.callback = cb
+  }
+
+  this.handleNavData = function(data){
+    if (data.demo == null) return;
+
+    var currentLat = data.gps.latitude
+    var currentLon = data.gps.longitude
+    console.log('lat/lon:', currentLat, currentLon);
+
+    var currentYaw = data.demo.rotation.yaw;
+
+    if (targetLat == null || targetLon == null || currentYaw ==  null || currentLat == null || currentLon == null) return;
+
+    var bearing = vincenty.distVincenty(currentLat, currentLon, targetLat, targetLon)
+
+    if(bearing.distance > 0.5){
+      console.log('distance', bearing.distance)
+      console.log('bearing:', bearing.initialBearing)
+      this.targetYaw = bearing.initialBearing
+
+      console.log('currentYaw:', currentYaw);
+      var eyaw = targetYaw - currentYaw;
+      console.log('eyaw:', eyaw);
+
+      var uyaw = yawPID.getCommand(eyaw);
+      console.log('uyaw:', uyaw);
+
+      var cyaw = within(uyaw, -1, 1);
+      console.log('cyaw:', cyaw);
+
+      this.client.clockwise(cyaw)
+      this.client.front(0.2)
+    } else {
+      this.targetYaw = null
+      this.targetLat = null;
+      this.targetLon = null;
+      this.client.stop()
+      this.callback()
+      console.log('Reached ', targetLat, targetLon)
+    }
+  }
+  this.client.on('navdata', this.handleNavData);
+}
+
+var gps = new gpsNavigator(client)
+
+gps.waypoint(51.392059, -2.3224394, function(){
+  console.log('done!')
+})
+
+
+
+client.on('navdata', handleNavData);
 
---
+++ server.js
@@ -0,0 +1,113 @@
+var express   = require('express')
+  , app       = express()
+  , server    = require('http').createServer(app)
+  , io        = require('socket.io').listen(server);
+
+app.use(express.static(__dirname + '/public'));
+app.use(app.router);
+
+app.get('/', function(req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+app.get('/phone', function(req, res) {
+  res.sendfile(__dirname + '/phone.html');
+});
+
+server.listen(8080);
+
+io.sockets.on('connection', function(socket) {
+  console.log('connection')
+
+  socket.on('takeoff', function(data){
+    console.log('takeoff', data)
+    client.takeoff()
+  })  
+  
+  socket.on('land', function(data){
+    console.log('land', data)
+    client.land()
+  })  
+  
+  socket.on('phone', function(data){
+    console.log('phone', data)
+    targetLat = data.lat
+    targetLon = data.lon
+    phoneAccuracy = data.accuracy
+  })  
+  
+  socket.on('stop', function(data){
+    console.log('stop', data)
+    targetYaw = null
+    targetLat = null
+    targetLon = null
+    client.stop()
+  })  
+
+  setInterval(function(){
+    io.sockets.emit('position', {lat: currentLat, lon: currentLon, distance: currentDistance})
+    io.sockets.emit('drone', {lat: currentLat, lon: currentLon, yaw: currentYaw})
+    io.sockets.emit('phone', {lat: targetLat, lon: targetLon, accuracy: phoneAccuracy})
+  },1000)
+  
+});
+
+var arDrone  = require('ar-drone');
+var PID      = require('./PID');
+var vincenty = require('node-vincenty');
+
+var yawPID = new PID(1.0, 0, 0.30);
+var client = arDrone.createClient();
+
+client.config('general:navdata_demo', 'FALSE');
+
+var targetLat, targetLon, targetYaw, cyaw, currentLat, currentLon,currentDistance, currentYaw, phoneAccuracy;
+
+var handleNavData = function(data){
+  if ( data.demo == null || data.gps == null) return;
+  
+  currentLat = data.gps.latitude
+  currentLon = data.gps.longitude
+
+  currentYaw = data.demo.rotation.yaw;
+
+  if (targetLat == null || targetLon == null || currentYaw ==  null || currentLat == null || currentLon == null) return;
+
+  var bearing = vincenty.distVincenty(currentLat, currentLon, targetLat, targetLon)
+
+  if(bearing.distance > 1){
+    currentDistance = bearing.distance
+    console.log('distance', bearing.distance)
+    console.log('bearing:', bearing.initialBearing)
+    targetYaw = bearing.initialBearing
+
+    console.log('currentYaw:', currentYaw);
+    var eyaw = targetYaw - currentYaw;
+    console.log('eyaw:', eyaw);
+
+    var uyaw = yawPID.getCommand(eyaw);
+    console.log('uyaw:', uyaw);
+
+    var cyaw = within(uyaw, -1, 1);
+    console.log('cyaw:', cyaw);
+
+    client.clockwise(cyaw)
+    client.front(0.05)
+  } else {
+    targetYaw = null
+    client.stop()
+    console.log('Reached ', targetLat, targetLon)
+  }
+}
+
+client.on('navdata', handleNavData);
+
+function within(x, min, max) {
+  if (x < min) {
+      return min;
+  } else if (x > max) {
+      return max;
+  } else {
+      return x;
+  }
+}
